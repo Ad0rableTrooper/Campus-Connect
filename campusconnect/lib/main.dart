53d5aa0b12891d9880595fc87fc142aef8022488
Adorable trooper <67198857+adorabletrooper@users.noreply.github.com>
2023-03-18 13:54:22 +0530

    New Commits

--- campusconnect/lib/main.dart
+++ campusconnect/lib/main.dart
@@ -22,9 +22,10 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Campus Connect'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -96,11 +97,14 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'You have pushed the plus button this many times:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              'A Jin Productions',
             ),
           ],
         ),
